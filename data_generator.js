a225bb59fb2738fb7e977d791411f5ebac1eba19
jonathan seneris <jonathanseneris@mbp.att.net>
2015-04-05 14:53:02 -0700

    everything works except userpage/load bug. top, input needs restyling.

--- data_generator.js
+++ data_generator.js
@@ -22,8 +22,8 @@ var randomElement = function(array) {
     var randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 };
-// random tweet generator
-var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+// random tweet generator    //ADDED SPACES TO OPENING TO FIX ALIGNMENT
+var opening = [' just', '', '', '', '', ' ask me how i', ' completely', ' nearly', ' productively', ' efficiently', ' last night i', ' the president', ' that wizard', ' a ninja', ' a seedy old man'];
 var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
